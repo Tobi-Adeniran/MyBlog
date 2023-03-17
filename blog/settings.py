86262e2ffb9dd82316ebc9d3a5cfcfde3d956692
Tobi-Adeniran <53083184+Tobi-Adeniran@users.noreply.github.com>
2023-03-17 13:35:57 +0100

    ad .env file and gitignore file

--- blog/settings.py
+++ blog/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_!xem08@*mh9j$-&_87-+kg&7kbvsm_d06p3mdc!u(*ndc*s='
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
