8e6c23283da1be84f8767d9bf7e83da862b160af
Tobi-Adeniran <53083184+Tobi-Adeniran@users.noreply.github.com>
2023-03-18 12:38:27 +0100

    add allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['stark-dawn-44205.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000', 'tobisblogg.online']
+ALLOWED_HOSTS = [' fast-cove-13198.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000', 'tobisblogg.online']
 
 
 # Application definition
