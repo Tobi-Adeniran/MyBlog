13bf8f285d3890d47eb0b1384ae22a6e47052976
Tobi-Adeniran <53083184+Tobi-Adeniran@users.noreply.github.com>
2023-03-18 12:43:14 +0100

    add debug true and allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [' fast-cove-13198.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000', 'tobisblogg.online']
+ALLOWED_HOSTS = ['fast-cove-13198.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000', 'tobisblogg.online']
 
 
 # Application definition
